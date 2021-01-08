136f593eb70593c2755d5eb2db4d4de4a9c032f8
StefanDC <77051592+StefanDC@users.noreply.github.com>
2021-01-08 15:32:25 +0100

    Work

--- MeinTransportApp/Form1.Designer.cs
+++ MeinTransportApp/Form1.Designer.cs
@@ -28,20 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.standort = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dauer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Umsteigen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gleis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Preis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(184, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.standort,
+            this.Dauer,
+            this.Umsteigen,
+            this.Gleis,
+            this.Preis});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(678, 309);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // standort
+            // 
+            this.standort.HeaderText = "standort";
+            this.standort.MinimumWidth = 6;
+            this.standort.Name = "standort";
+            this.standort.Width = 125;
+            // 
+            // Dauer
+            // 
+            this.Dauer.HeaderText = "Dauer";
+            this.Dauer.MinimumWidth = 6;
+            this.Dauer.Name = "Dauer";
+            this.Dauer.Width = 125;
+            // 
+            // Umsteigen
+            // 
+            this.Umsteigen.HeaderText = "Umsteigen";
+            this.Umsteigen.MinimumWidth = 6;
+            this.Umsteigen.Name = "Umsteigen";
+            this.Umsteigen.Width = 125;
+            // 
+            // Gleis
+            // 
+            this.Gleis.HeaderText = "Gleis";
+            this.Gleis.MinimumWidth = 6;
+            this.Gleis.Name = "Gleis";
+            this.Gleis.Width = 125;
+            // 
+            // Preis
+            // 
+            this.Preis.HeaderText = "Preis";
+            this.Preis.MinimumWidth = 6;
+            this.Preis.Name = "Preis";
+            this.Preis.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 469);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn standort;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dauer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Umsteigen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gleis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Preis;
     }
 }
 
--- MeinTransportApp/Form1.cs
+++ MeinTransportApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,72 @@ namespace MeinTransportApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          //  .Columns[0].Name = "Luzern";
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.AddRange(new object[] {"Luzern",
+                        "Item 2",
+                        "Item 3",
+                        "Item 4",
+                        "Item 5"});
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var transport = new Transport();
+            var connections = transport.GetConnections("Zürich", "Sursee").ConnectionList;
+
+            // dataGridView1 ist ein Objekt.
+            // Alle die mit einem Schraubschlüssel neben den Namen sind Eigenschaften von
+            // dataGridView1.
+            // Also:
+            // objekt.Eigenschaft
+
+
+            foreach(var x in connections)
+            {
+                // Das hier, dafür haben wir das herausgesucht
+                // x.From.Station.Name
+                dataGridView1.Rows.Add(new string[]
+                {
+                    x.From.Station.Name,
+                    x.To.Station.Name,
+                    x.To.Station.Name,
+                    x.To.Station.Name,
+                    x.To.Station.Name,
+                });
+            }
+
+            var myArray = new int[] { 1, 2 };
+            foreach (var x in myArray)
+            {
+                Console.WriteLine(x);
+            }
+
+            dataGridView1.Rows.Add(new[]
+ {
+     comboBox1.Text,
+     "Reinach", 
+     "Beromünster",
+     "Sursee",
+     "Menziken",
+     // "Luzern",
+                
+     
+     
+}) ;
         }
     }
 }
